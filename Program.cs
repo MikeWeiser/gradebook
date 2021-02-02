e16dd4673e7f9ac59965df135fb3840718d82538
MikeWeiser <mike.weiser.programs@gmail.com>
2021-02-02 13:57:15 -0600

    double math

--- Program.cs
+++ Program.cs
@@ -7,6 +7,13 @@ namespace GradeBook
         //blah blah
         static void Main(string[] args)
         {
+
+            double x = 34.1;
+            double y = 77.9;
+
+            double result = x + y;
+
+            Console.WriteLine(result);
        
             if (args.Length > 0)
             {
