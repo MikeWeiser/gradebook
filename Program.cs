1c5793ef67791031b3ffa21fb80bc304515dc51d
MikeWeiser <mike.weiser.programs@gmail.com>
2021-02-02 13:51:00 -0600

    IfElse Feb2

--- Program.cs
+++ Program.cs
@@ -7,10 +7,7 @@ namespace GradeBook
         //blah blah
         static void Main(string[] args)
         {
-            //string concatentation:
-            //Console.WriteLine("Hello" + args[0] + "!");
-
-            //string interpolation:
+       
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
